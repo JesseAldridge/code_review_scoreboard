a3a49cf70f86c008f72590797b004f120246ff49
Jesse Aldridge <JesseAldridge@gmail.com>
2016-02-03 13:01:29 -0800

    add slack integration

--- main.py
+++ main.py
@@ -5,19 +5,21 @@ import requests
 
 import secrets
 
+testing = False
+
 username = 'JesseAldridge'
 repo_url = 'https://api.github.com/repos/gigwalk-corp/gigwalk_apps_platform_api'
 
 
 auth_ = auth.HTTPBasicAuth(username, secrets.github_api_key)
 name_to_count = {}
-for page in range(1, 5):
+for page in range(1, 3) if testing else range(1, 5):
     print 'page:', page
 
     main_resp = requests.get('{}/pulls?state=all&page={}'.format(repo_url, page), auth=auth_)
     all_prs = json.loads(main_resp.content)
 
-    for pr in all_prs:
+    for pr in all_prs[:2] if testing else all_prs:
         created_by = pr['user']['login']
         print '  pr by:', created_by
 
@@ -34,4 +36,18 @@ for page in range(1, 5):
                 name_to_count.setdefault(name, 0)
                 name_to_count[name] += 1
 
-print sorted(name_to_count.iteritems(), key=lambda t: -t[-1])
+results = list(sorted(name_to_count.iteritems(), key=lambda t: -t[-1]))
+print 'results:', results
+
+slack_str = (
+    'Code review scoreboard: {};  Good job {}!  '
+    'https://github.com/JesseAldridge/code_review_scoreboard'.format(
+        results, results[0][0] if results and results[0] else 'nobody'))
+
+print 'slack_str:', slack_str
+
+url = (
+    'https://slack.com/api/chat.postMessage?token={}'
+    '&channel=%23_eng_backend&text={}&pretty=1').format(
+    secrets.slack_api_key, urllib.quote(message))
+requests.post(url)
