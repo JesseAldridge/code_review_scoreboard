b15f7721ebe9470745f54236eb1ed976d8ca96ca
Jesse Aldridge <JesseAldridge@gmail.com>
2016-11-16 12:02:51 -0800

    planning for getting comments since date

--- temp_recent_comments.py
+++ temp_recent_comments.py
@@ -8,26 +8,56 @@ import secrets
 
 auth_ = auth.HTTPBasicAuth('JesseAldridge', secrets.github_api_key)
 
-for page in range(20):
-  print page
-  # url = ('https://api.github.com/repos/gigwalk-corp/gigwalk_apps_platform_api/issues/comments'
-  #        '?sort=created&direction=desc&page={}').format(page)
 
-  # YYYY-MM-DDTHH:MM:SSZ
+# Top-level comments(?)
 
-  since_str = (datetime.utcnow() - timedelta(days=14)).strftime('%Y-%m-%dT%H:%M:%SZ')
+since_str = (datetime.utcnow() - timedelta(days=14)).strftime('%Y-%m-%dT%H:%M:%SZ')
 
-  url = ('https://api.github.com/repos/gigwalk-corp/gigwalk_apps_platform_api/pulls/comments'
-         '?sort=created&direction=desc&since={}&page={}').format(since_str, page)
+url = ('https://api.github.com/repos/gigwalk-corp/gigwalk_apps_platform_api/pulls/comments'
+       '?sort=created&direction=desc&since={}&page={}').format(since_str, 1)
 
-  resp = requests.get(url, auth=auth_)
-  prs_list = json.loads(resp.content)
-  if not prs_list:
-    print "that's all of them"
-    break
-  print prs_list[-1]['created_at']
+resp = requests.get(url, auth=auth_)
+prs_list = json.loads(resp.content)
 
-  json.dumps(json.loads(resp.content), indent=2)
-  if 'Do we want some kind of error reporting' in resp.content:
-    print 'found it'
+print prs_list[-1]['created_at']
+# for pr_d in prs_list:
+#   print pr_d['body']
 
+
+# Inline comments(?)
+
+since_str = (datetime.utcnow() - timedelta(days=14)).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+url = ('https://api.github.com/repos/gigwalk-corp/gigwalk_apps_platform_api/comments'
+       '?sort=created&direction=desc&since={}&page={}').format(since_str, 1)
+
+print 'url:', url
+
+resp = requests.get(url, auth=auth_)
+prs_list = json.loads(resp.content)
+
+print prs_list[-1]['created_at']
+for pr_d in prs_list:
+  print pr_d['body']
+
+
+# for page in range(1):
+#   print page
+#   # url = ('https://api.github.com/repos/gigwalk-corp/gigwalk_apps_platform_api/issues/comments'
+#   #        '?sort=created&direction=desc&page={}').format(page)
+
+#   # YYYY-MM-DDTHH:MM:SSZ
+
+#   since_str = (datetime.utcnow() - timedelta(days=14)).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+#   url = ('https://api.github.com/repos/gigwalk-corp/gigwalk_apps_platform_api/pulls/comments'
+#          '?sort=created&direction=desc&since={}&page={}').format(since_str, page)
+
+#   resp = requests.get(url, auth=auth_)
+#   prs_list = json.loads(resp.content)
+#   if not prs_list:
+#     print "that's all of them"
+#     break
+#   print prs_list[-1]['created_at']
+
+#   print json.dumps(json.loads(resp.content), indent=2)
